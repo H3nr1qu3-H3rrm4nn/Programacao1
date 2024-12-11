168f3d76980ae6f16dfab45d070948cb24f2fe48
Henrique Franco Herrmann <141282727+H3nr1qu3-H3rrm4nn@users.noreply.github.com>
2024-12-11 20:11:34 -0300

    11-12-2024

--- aula_13-11-2024/aula_13_11_2024/src/main/java/com/DAO/ClienteDAO.java
+++ aula_13-11-2024/aula_13_11_2024/src/main/java/com/DAO/ClienteDAO.java
@@ -1,5 +1,12 @@
 package com.DAO;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 import com.model.Cliente;
 
 public class ClienteDAO {
@@ -7,6 +14,7 @@ public class ClienteDAO {
     public boolean salvar(Cliente cliente){
 
         System.out.println("Eu salvei o cliente "+ cliente.getNome()); 
+        geraArquivo(cliente);
         return true;
     }
     public void alterar(){
@@ -14,5 +22,29 @@ public class ClienteDAO {
     }
     public void excluir(){
         System.out.println("Executei o método excluir do DAO");    
+    }
+    
+
+    private static void geraArquivo(Cliente cliente){
+        String caminhoArquivo = "prog1.txt";
+
+        File arquivo = new File(caminhoArquivo);
+
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter(arquivo, true))){
+            writer.write("Nome: "+ cliente.getNome()+"\n");
+            writer.write("Endereço: "+ cliente.getEndereco() +"\n");
+            writer.write("CPF: "+ cliente.getCpf() + "\n");
+            writer.write("RG: "+ cliente.getRg()+ "\n");
+            JOptionPane.showMessageDialog(null,
+                    "Dados do cliente "+ cliente.getNome() + " gravando com sucesso","Arquivo",1);
+            } catch (IOException e){
+                JOptionPane.showMessageDialog(null,
+                    "Erro ao gravar dados.",
+                    "Erro",
+                    0);
+                e.printStackTrace();
+            }
+        
     }
 }
